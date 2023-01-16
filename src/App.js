449dc8c2773102bb3aa08d93ef49f9044bef7107
Michael Budei <77558463+MikeBlackwood@users.noreply.github.com>
2023-01-15 19:09:16 -0800

    Created reusable component for Dropdown

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
     const options = [ {label: 'Red', value: 'red'}, {label: 'Green', value: 'green'}, {label: 'Blue', value: 'blue'}]
     const [selection, setSelection] = useState(null)
     const handleSelectOption = (option) => {
-        setSelection(option.label)
+        setSelection(option)
     }
   return (
     <div className="App">
-        <Dropdown options={options} onSelect={handleSelectOption} selected={selection}/>
+        <Dropdown options={options} onChange={handleSelectOption} value={selection}/>
     </div>
   );
 }
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,27 +1,45 @@
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
+import {GoChevronDown} from "react-icons/go";
+import Panel from "./Panel";
 
-const Dropdown = ({onSelect, selected, options}) => {
-    const [isOpen, setIsOpen] = useState(false)
-    let content = 'Select...';
-    if(selected)
-    {
-        content = selected;
-    }
+const Dropdown = ({onChange, value, options}) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const dropdownRef = useRef();
+
+    useEffect(() => {
+        const handler = (event) => {
+            if(!dropdownRef.current)
+            {
+                return;
+            }
+            if (!dropdownRef.current.contains(event.target))
+            {
+                setIsOpen(false)
+            }
+        }
+        document.addEventListener('click', handler)
+        return () => {
+            document.removeEventListener('click', handler);
+        }
+    }, [])
     const handleClick = () => {
         setIsOpen(!isOpen)
     }
     const handleOptionClick = (option) => {
-        onSelect(option)
+        onChange(option)
         setIsOpen(false)
     }
     const renderedOptions = options.map((option, index) => {
-        return <div onClick={ () => handleOptionClick(option)} key={option.value}>{option.label}</div>
+        return <div className='hover:bg-sky-100 rounded cursor-pointer p-1' onClick={ () => handleOptionClick(option)} key={option.value}>{option.label}</div>
     })
 
     return(
-        <div>
-            <div onClick={handleClick}>{content}</div>
-            {isOpen && <div>{renderedOptions}</div>}
+        <div className='w-48 relative' ref={dropdownRef}>
+            <Panel className='flex justify-between cursor-pointer items-center w-full'
+                   onClick={handleClick}>{value?.label || 'Select...'}
+                <GoChevronDown/>
+            </Panel>
+            {isOpen && <Panel className='absolute top-full'>{renderedOptions}</Panel>}
         </div>
     )
 }
